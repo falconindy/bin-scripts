e4d8d68dd921ad990896a0efdc6d800aa63e8870
Dave Reisner <haruko@quake.localdomain>
2009-12-13 00:48:51 -0500

    remove yaourt dependency

--- latest-chromium.sh
+++ latest-chromium.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[[ "$1" = "yes" ]] && yaourt -S chromium-browser-bin && exit
+[[ "$1" = "yes" ]] && cd ~/abs/chromium-browser-bin/ && makepkg -fi && exit 0
 
 echo "Currently using build: `pacman -Qi chromium-browser-bin | grep Version | cut -d: -f2`"
 echo "Latest build available: `curl --silent http://build.chromium.org/buildbot/snapshots/chromium-rel-linux-64/LATEST`"
