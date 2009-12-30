43c9f64e235ef11c0dc883ec8c033bf2dcee5013
Dave Reisner <haruko@quake.localdomain>
2009-12-29 20:38:11 -0500

    tee *some* output to screen for running interactively

--- s3bkup.sh
+++ s3bkup.sh
@@ -12,13 +12,12 @@ exec >> $LOGFILE #redirect all to log
 
 # Start logging
 echo "-------------------------------------------------"
-echo "Backup starting at `date +'%D %T'` with parameter: $1"    # timestamp this bad agent
+echo "Backup starting at `date +'%D %T'` with parameter: $1" | tee -a $LOGFILE >&6
 
 # Pre-emptively declare ending procedure since we're not sure when we're exiting (in case of error)
 finish() {
 	[[ -f "$EXCLUSION_LIST" ]] && rm "$EXCLUSION_LIST"	# remove our temp file for exclusions if it exists
-	echo "Backup ended at `date +'%D %T'`"	# timestamp this bad agent
-	echo >> $LOGFILE
+	echo "Backup ended at `date +'%D %T'`" | tee -a $LOGFILE >&6
     exec 1>&6 6>&- #STDOUT back to STDOUT and destroy descriptor 6
 	exit
 }
@@ -48,30 +47,28 @@ grep -vE "^#" $1 | sed -n '/<exclude>/,/<\/exclude>/p' | grep -vE "</?exclude>" 
 OPTIONS=("-Rua" "--delete" "--stats" "--exclude-from=$EXCLUSION_LIST")
 COMMAND=("rsync" "${OPTIONS[@]}" "${INCLUDES[@]}" "$DESTINATION")
 
-echo "Executing rsync as:"
-echo "  ${COMMAND[@]}"
+echo "Executing rsync as: ${COMMAND[@]}" | tee -a $LOGFILE >&6
 
 # Enough talk. Fucking do it already.
 "${COMMAND[@]}"
 
 #Explanation of arguments in rsync
-#	-R = use relative path names
-#	-u = skip files that are newer on the receiver
-#	-a = equivalent of -rlptgoD
-#	  -r = recurse into directories
-#	  -l = copy symlinks as symlinks
-#	  -p = preserve permissions
-#	  -t = preserve modification times
-#	  -g = preserve group
-#	  -o = preserve owner
-#	  -D = preserve special and device files
-#	--stats = give some file-transfer stats
-#	--delete = delete extraneous files from destination dirs
-#	--size-only = skip files that match in size
-#	--exclude-from = path to our exclusion file
+#   -R = use relative path names
+#   -u = skip files that are newer on the receiver
+#   -a = equivalent of -rlptgoD
+#     -r = recurse into directories
+#     -l = copy symlinks as symlinks
+#     -p = preserve permissions
+#     -t = preserve modification times
+#     -g = preserve group
+#     -o = preserve owner
+#     -D = preserve special and device files
+#   --stats = give some file-transfer stats
+#   --delete = delete extraneous files from destination dirs
+#   --size-only = skip files that match in size
+#   --exclude-from = path to our exclusion file
 #
-#	NOTE: -n can be added to the execution to create a dry-run!
+# NOTE: -n can be added to the execution to create a dry-run!
 
-# All done! Wrap it up, yo. Hopefully we got here with great success.
 finish
 
