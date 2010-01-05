15603c0d55ddf3fcfe236b603979aeeba047fad2
Dave Reisner <haruko@quake.localdomain>
2010-01-04 21:28:10 -0500

    Update for -svn pkg

--- latest-chromium.sh
+++ latest-chromium.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[[ "$1" = "yes" ]] && cd ~/abs/chromium-browser-bin/ && makepkg -fi && exit 0
+[[ "$1" = "yes" ]] && cd ~/abs/chromium-browser-svn/ && makepkg -firs && exit 0
 
 echo "Currently using build: `pacman -Qi chromium-browser-svn | grep Version | cut -d: -f2`"
 echo "Latest build available: `curl --silent http://build.chromium.org/buildbot/continuous/linux/LATEST/REVISION`"
