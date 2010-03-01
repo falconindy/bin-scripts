94a24b0e5db511f3864b965bc43fa0df9b495191
Dave Reisner <d@falconindy.com>
2010-03-01 09:46:47 -0500

    Die evil tabs

--- findMount.sh
+++ findMount.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 for mount in $(mount | awk '{for(i=3;i<NF-2;i++) printf "%s ",$i;print '\n'}'); do
-	result=($(pwd -P | grep -o "${mount[*]}"))
-	if [[ $(expr length "${result[*]}") -gt $(expr length "${mount_point[*]}") ]]; then
-		mount_point="$result"
-	fi
+    result=($(pwd -P | grep -o "${mount[*]}"))
+    if [[ $(expr length "${result[*]}") -gt $(expr length "${mount_point[*]}") ]]; then
+        mount_point="$result"
+    fi
 done
 
 echo $mount_point
