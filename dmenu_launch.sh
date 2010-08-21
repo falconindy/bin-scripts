1663df633e10afa929235df19036f7f4c7c618e7
Dave Reisner <d@falconindy.com>
2010-08-20 21:30:15 -0400

    dmenu_launch: extend to catch terminal apps which need to be run through a terminal

--- dmenu_launch.sh
+++ dmenu_launch.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
 font="-*-nu-*-*-*-*-11-*-*-*-*-*-*-*"
+term=urxvtc
 normbgcolor="#303030"
 normfgcolor="#FFFFFF"
 selbgcolor="#3465A4"
 selfgcolor="#FFFFFF"
 
-exec dmenu_run -i -b -fn $font -nb $normbgcolor -nf $normfgcolor -sb $selbgcolor -sf $selfgcolor
+cmd=$(dmenu_path | dmenu -i -b -fn $font -nb $normbgcolor -nf $normfgcolor -sb $selbgcolor -sf $selfgcolor)
+
+case ${cmd%% *} in
+  ncmpcpp|htop|vim)   exec $term -name $cmd -e $cmd ;;
+  tmux)               exec $term -name tmux -geometry 122x77 -e tmux -L main attach ;;
+  *)                  exec $cmd
+esac
