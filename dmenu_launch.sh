e77338f5499a078c202b3b23fb7a5e706fe2cca8
Dave Reisner <d@falconindy.com>
2010-09-14 06:28:20 -0400

    dmenu_launch: remove superfluous basecmd variable

--- dmenu_launch.sh
+++ dmenu_launch.sh
@@ -8,10 +8,9 @@ selbgcolor="#3465A4"
 selfgcolor="#FFFFFF"
 
 cmd=$(dmenu_path | dmenu -i -b -fn $font -nb $normbgcolor -nf $normfgcolor -sb $selbgcolor -sf $selfgcolor)
-basecmd=${cmd%% *}
 
 case ${basecmd} in
-  ncmpcpp|htop|vim)   exec $term -name $basecmd -e $basecmd ;;
+  ncmpcpp|htop|vim)   exec $term -name $cmd -e $cmd ;;
   tmux)               exec $term -name tmux -geometry 122x77 -e tmux -L main attach ;;
   *)                  exec $cmd
 esac
